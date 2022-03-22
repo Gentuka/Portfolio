8ecc1d6bd001a5d55d92d3f2b65e4ddb0d8f8907
Gentuka <gent-uka@gmx.de>
2022-03-22 12:59:26 +0100

    responsive first

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
-window.addEventListener("load", () => {
-// dein code
-});
+function hamburger() {
+    var navItems = document.getElementById('navItems');
+    
+    if (navItems.classList.contains('display-none')){
+        navItems.classList.remove('display-none');
+    } else {
+        navItems.classList.add('display-none');
+    }
+
+
+}
