053635f79e2fefcb89027fd92cea5d18e74b5fb0
Gentuka <gent-uka@gmx.de>
2022-06-05 18:34:26 +0200

    kleine Anpassungen und work.html ist drinnen

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,29 +1,41 @@
-function about(){
-    var element = document.getElementById('text')
-    var aboutText = 'Hallo, ich bin Gent';
+function basic(){
+    var element = document.getElementById('text');
 
     element.innerText = "$ ";
 
     counter = 0;
 
     setTimeout(function() {
-        addText(counter);
+        addText(counter, "Basic", 'text');
     }, 100)
-    // addText(counter);
 }
 
-function addText(counter){
-    var element = document.getElementById('text')
-    var aboutText = 'Hallo, ich bin Gent';
+function about(){
+    var element = document.getElementById('text');
+
+    element.innerText = "$ ";
+
+    counter = 0;
+
+    setTimeout(function() {
+        addText(counter, "Hallo, ich bin Gent und mache meine Ausbilung als Anwendungsentwickler", 'text');
+    }, 100)
+}
+
+function work(){
+    
+}
+
+function addText(counter, aboutText, id){
+    var element = document.getElementById(id)
+    var text = aboutText;
     var random = Math.floor(Math.random() * 150);
 
-    if (counter < aboutText.length){
-        element.append(aboutText[counter]);
-        console.log(counter);
-        counter++;
-
-        setTimeout(function() {
-            addText(counter);
-        }, random)
-    }
+        if (counter < aboutText.length){
+            element.append(aboutText[counter]);
+            counter++;
+            setTimeout(function() {
+                addText(counter, text, id);
+            }, random)
+        } 
 }
